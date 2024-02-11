7a40ebafc34ac65750bd4ab09671da6374dd7430
Hussein Issa <hussein.issa.op@gmail.com>
2024-02-11 18:42:19 +0200

    first commit

---
+++ Task2 Portfolio/submit_form.php
@@ -0,0 +1,17 @@
+<?php
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"])) {
+//         echo "Error: Please fill out all required fields.";
+//     } else {
+//         if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+//             echo "Error: Please enter a valid email address.";
+//         } else {
+//             $phone = $_POST["phone"];
+//             if (!empty($phone) && !ctype_digit($phone)) {
+//                 echo "Error: Phone number should contain only numbers.";
+//              }
+//         }
+//     }
+// }
+?>
+
